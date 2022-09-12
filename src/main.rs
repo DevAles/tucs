bea6e0343853a6039e6427ccfbac4d7653db224b
DevAles <devalesoficial@gmail.com>
2022-09-12 14:04:10 -0300

    Add a Welcome text

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@
 
 use core::panic::PanicInfo;
 
+static OPALA: &[u8] = b"Welcome to TUCS!";
+
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
@@ -10,5 +12,14 @@ fn panic(_info: &PanicInfo) -> ! {
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (index, &byte) in OPALA.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(index as isize * 2) = byte;
+            *vga_buffer.offset(index as isize * 2 + 1) = 0xf;
+        }
+    }
+
     loop {}
 }
