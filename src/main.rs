9a9edbaa26078f9264fc128e0681e27c8c3d2146
DevAles <devalesoficial@gmail.com>
2022-10-13 21:12:02 -0300

    Implement test runner

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 #![no_std]
 #![no_main]
 
+
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::tester)]
+
 pub mod vga;
 
 use core::panic::PanicInfo;
@@ -33,4 +37,14 @@ pub extern "C" fn _start() -> ! {
     println!("{}", WELCOME_MESSAGE);
 
     loop {}
+}
+
+
+#[cfg(test)]
+fn tester(tests: &[&dyn Fn()]) {
+    println!("> Running {} tests", tests.len());
+
+    for test in tests {
+        test();
+    }
 }
